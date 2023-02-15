b690d288ff6696b55c3d27dbfafd1bc7b4582414
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-02-15 11:28:15 +0100

    Upload

---
+++ Skola12/Skola12/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Skola12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string fName = "", obsah = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                ListBox lb = new ListBox();
+                lb.Width = 200;
+                lb.Height = 130;
+                lb.Location = new Point(20, 20);
+                lb.Enabled = true;
+                lb.Visible = true;
+                this.Controls.Add(lb);
+                fName = file.FileName;
+                StreamReader ctenar = new StreamReader(fName, Encoding.GetEncoding("windows-1250"));
+                while (!ctenar.EndOfStream)
+                    lb.Items.Add(ctenar.ReadLine());
+                ctenar.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader ctenar = new StreamReader(fName, Encoding.GetEncoding("windows-1250"));
+            obsah = ctenar.ReadToEnd().ToString();
+            ctenar.Close();
+            StreamWriter zapis = new StreamWriter(fName, false, Encoding.GetEncoding("windows-1250"));
+            if (comboBox1.SelectedItem.ToString() == "Velká na malá")
+            {
+                zapis.Write(obsah.ToLower());
+            } else if(comboBox1.SelectedItem.ToString() == "Malá na velká")
+            {
+                zapis.Write(obsah.ToUpper());
+            } else
+                MessageBox.Show("Musíte něco vybrat");
+            zapis.Close();
+            ctenar = new StreamReader(fName, Encoding.GetEncoding("windows-1250"));
+            ListBox ld = new ListBox();
+            foreach(Control c in this.Controls)
+            {
+                if (c.GetType() == Type.GetType("ListBox"))
+                    ld = (ListBox)c;
+            }
+            ld.Items.Clear();
+            while (!ctenar.EndOfStream)
+                ld.Items.Add(ctenar.ReadLine());
+            ctenar.Close();
+        }
+
+
+    }
+}
---
+++ Skola12/Skola12/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Skola12
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable tabulka = new DataTable();
+            StreamReader ctenar = new StreamReader("knihy.txt", Encoding.GetEncoding("windows-1250"));
+            string prvni = ctenar.ReadLine();
+            foreach (string data in prvni.Split(';')) 
+                tabulka.Columns.Add(data);
+            while (!ctenar.EndOfStream)
+            {
+                tabulka.Rows.Add(ctenar.ReadLine().Split(';'));
+            }
+            ctenar.Close();
+            StreamWriter zapis1 = new StreamWriter("knihy1.txt", true, Encoding.GetEncoding("windows-1250"));
+            StreamWriter zapis2 = new StreamWriter("knihy2.txt", true, Encoding.GetEncoding("windows-1250"));
+
+            zapis1.WriteLine(prvni);
+            zapis2.WriteLine(prvni);
+            foreach (DataRow row in tabulka.Rows)
+            {
+                if (int.Parse(row.ItemArray[row.ItemArray.Length-1].ToString()) > 1950)
+                    zapis1.WriteLine(string.Join(";", row.ItemArray));
+                else
+                    zapis2.WriteLine(string.Join(";", row.ItemArray));
+            }
+            zapis1.Close();
+            zapis2.Close();
+            dataGridView1.DataSource = tabulka;
+        }
+    }
+}
